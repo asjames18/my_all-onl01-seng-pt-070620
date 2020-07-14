5b8ad7d0bd0acadeaebda00026105830918617bd
Antonio James <asjames18@gmail.com>
2020-07-14 19:00:52 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
+i = 0
+while i < collection.length
+  yield(collection {|i| i < 3})
+
+  i += 1
+end
+# end
+# if collection.include?(false)
+#     false
+#   else
+#     true
 
 end
